ba7744b0ec22fc4016d7e30412b775d63fbd4c5c
Nick Williams <133838383+Nick-Williamz@users.noreply.github.com>
2023-05-21 20:50:29 -0700

    removed test code

--- modules/document_loader.py
+++ modules/document_loader.py
@@ -9,7 +9,6 @@ def load_documents(document_dir=DOCUMENT_DIR):
         with open(filename, 'r', encoding='utf-8') as file:
             doc_text = file.read()
             if doc_text.strip():
-                print(f"Loaded document '{filename}' with length {len(doc_text)}:\n{doc_text}\n")
                 documents.append(doc_text)
             else:
                 print(f"Skipped empty document '{filename}'")
